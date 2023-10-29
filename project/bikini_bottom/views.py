73f3538cae53e3eb66fdb12ba4ee32c1d3d636df
aruzakiuiuiu <140229053+aruzakiuiuiu@users.noreply.github.com>
2023-10-29 14:10:31 +0700

    Step 3 - Configure Static & API

--- project/bikini_bottom/views.py
+++ project/bikini_bottom/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.core.serializers import serialize   # untuk membuat API
+from .models import Facility   # memanggil model Facility yang tadi dibuat
+from django.http import HttpResponse   # untuk menghasilkan response dari API
 
 # Create your views here.
 def home(request):
-      return render(request, 'pages/home.html');
+      return render(request, 'pages/home.html')
+
+def home_map_api(request):
+  data = serialize('geojson', Facility.objects.all()) 
+  return HttpResponse(data, content_type ='json') 
