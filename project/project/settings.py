7a885b9711fdf00a010b3223b82ab4ae9015692d
aruzakiuiuiu <140229053+aruzakiuiuiu@users.noreply.github.com>
2023-11-12 18:45:17 +0700

    Step 4 - Create Custom API

--- project/project/settings.py
+++ project/project/settings.py
@@ -128,10 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = 'assets/static'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "assets/static")
-]
+STATIC_ROOT = 'assets/static'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = 'assets/media'
 
